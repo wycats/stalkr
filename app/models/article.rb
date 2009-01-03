6449b8d4e0621c72753ebbdaa58a005f17bf6209
Yehuda Katz <wycats@gmail.com>
2009-01-03 12:34:36 -0800

    Step 23:  Set up timestamps and relationships for articles and comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,13 @@
 class Article
   include DataMapper::Resource
   
-  property :id, Serial
-
-  property :body,  Text,   :nullable => false
-  property :title, String, :nullable => false
+  # Database schema
+  property   :id,    Serial
+  property   :body,  Text,   :nullable => false
+  property   :title, String, :nullable => false
   timestamps :at
+  
+  # Relationships
+  has n, :comments
+
 end
