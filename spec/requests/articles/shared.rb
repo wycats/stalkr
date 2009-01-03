97adc7310472e14a5d937558e354a8e0d57bebc2
Yehuda Katz <wycats@gmail.com>
2009-01-03 12:16:13 -0800

    Step 19:  Inform the user why their creation or update failed validations

--- spec/requests/articles/shared.rb
+++ spec/requests/articles/shared.rb
@@ -27,3 +27,46 @@ shared_examples_for "a form for entering title and body" do
     @response.should have_selector("form input[type='submit']")
   end
 end
+
+shared_examples_for "a form with potentially invalid details" do
+  describe "without a title" do
+    before(:each) do
+      @paragraph = /[:paragraph:]/.gen
+      fill_in      "Title", :with => ""
+      fill_in      "Body",  :with => @paragraph
+      @response = click_button @button_name
+    end
+    
+    it "re-renders the text with errors" do
+      @response.should have_selector("div.error h2:contains('Form submission failed')")
+      @response.should have_selector("div.error li:contains('Title')")
+    end
+  end
+  
+  describe "without a body" do
+    before(:each) do
+      fill_in      "Title", :with => "Hello world"
+      fill_in      "Body",  :with => ""
+      @response = click_button @button_name
+    end
+    
+    it "re-renders the text with errors" do
+      @response.should have_selector("div.error h2:contains('Form submission failed')")
+      @response.should have_selector("div.error li:contains('Body')")
+    end
+  end
+  
+  describe "without a title or body" do
+    before(:each) do
+      fill_in      "Title", :with => ""
+      fill_in      "Body",  :with => ""
+      @response = click_button @button_name
+    end
+    
+    it "re-renders the text with errors" do
+      @response.should have_selector("div.error h2:contains('Form submission failed')")
+      @response.should have_selector("div.error li:contains('Title')")
+      @response.should have_selector("div.error li:contains('Body')")
+    end
+  end  
+end
