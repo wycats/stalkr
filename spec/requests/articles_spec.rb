4ebe0d9ecaa47cc040004dc48eb9481d3133d81e
Yehuda Katz <wycats@gmail.com>
2009-01-02 16:53:30 -0800

    Step 11:  Modify articles/show.html to show a single article, and add a "Back" link back to the index

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -111,6 +111,18 @@ describe "resource(@article)", :given => "a article exists" do
     it "responds successfully" do
       @response.should be_successful
     end
+    
+    it "has a title" do
+      @response.should have_selector("h1:contains('#{@article[:title]}')")
+    end
+    
+    it "has a body" do
+      @response.should have_selector(":contains('#{@article[:body]}')")
+    end
+    
+    it "has a link back to the index page" do
+      @response.should have_selector("a[href='#{resource(:articles)}']")
+    end
   end
   
   describe "PUT" do
