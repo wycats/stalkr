9b680f010f45b587ca53793641c56a5e5b642d59
Yehuda Katz <wycats@gmail.com>
2009-01-04 15:36:36 -0800

    Step 31:  Write mapping for legacy database for comments

--- lib/data_migration/legacy.rb
+++ lib/data_migration/legacy.rb
@@ -16,4 +16,22 @@ class Article
   property :body,       Text,     :field => 'post_content'
   property :created_at, DateTime, :field => 'post_date'
   property :updated_at, DateTime, :field => 'post_modified'
+  
+  has n, :comments
+end
+
+class Comment
+  include DataMapper::Resource
+  
+  # name of the table
+  storage_names[:default] = 'wp_ojqdyw_comments'
+  
+  property :id,         Serial,   :field => 'comment_ID'
+  property :article_id, Integer,  :field => 'comment_post_ID'
+  property :author,     String,   :field => 'comment_author'
+  property :body,       Text,     :field => 'comment_content'
+  property :created_at, DateTime, :field => 'comment_date'
+  property :updated_at, DateTime, :field => 'comment_date'
+  
+  belongs_to :article
 end
