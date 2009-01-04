ff13f5796a20aa70da42343890508bb4da54d1d9
Yehuda Katz <wycats@gmail.com>
2009-01-04 15:49:39 -0800

    Step 32:  Switched data migration models to use :legacy database

--- lib/data_migration/legacy.rb
+++ lib/data_migration/legacy.rb
@@ -1,4 +1,4 @@
-DataMapper.setup( :default, 
+DataMapper.setup( :legacy, 
                   :adapter => "mysql", 
                   :encoding => "utf8", 
                   :database => "legacy-blog", 
@@ -9,13 +9,19 @@ class Article
   include DataMapper::Resource
 
   # name of the table
-  storage_names[:default] = 'wp_ojqdyw_posts'
+  storage_names[:legacy] = 'wp_ojqdyw_posts'
   
-  property :id,         Serial
-  property :title,      String,   :field => 'post_title'
-  property :body,       Text,     :field => 'post_content'
-  property :created_at, DateTime, :field => 'post_date'
-  property :updated_at, DateTime, :field => 'post_modified'
+  property :id,           Serial
+  property :title,        String
+  property :body,         Text
+  timestamps :at
+  
+  repository(:legacy) do
+    property :title,      String,   :field => 'post_title'
+    property :body,       Text,     :field => 'post_content'
+    property :created_at, DateTime, :field => 'post_date'
+    property :updated_at, DateTime, :field => 'post_modified'
+  end
   
   has n, :comments
 end
@@ -24,14 +30,22 @@ class Comment
   include DataMapper::Resource
   
   # name of the table
-  storage_names[:default] = 'wp_ojqdyw_comments'
+  storage_names[:legacy] = 'wp_ojqdyw_comments'
+
+  property :id,           Serial
+  property :article_id,   Integer
+  property :author,       String
+  property :body,         Text
+  timestamps :at
   
-  property :id,         Serial,   :field => 'comment_ID'
-  property :article_id, Integer,  :field => 'comment_post_ID'
-  property :author,     String,   :field => 'comment_author'
-  property :body,       Text,     :field => 'comment_content'
-  property :created_at, DateTime, :field => 'comment_date'
-  property :updated_at, DateTime, :field => 'comment_date'
+  repository(:legacy) do
+    property :id,         Serial,   :field => 'comment_ID'
+    property :article_id, Integer,  :field => 'comment_post_ID'
+    property :author,     String,   :field => 'comment_author'
+    property :body,       Text,     :field => 'comment_content'
+    property :created_at, DateTime, :field => 'comment_date'
+    property :updated_at, DateTime, :field => 'comment_date'
+  end
   
   belongs_to :article
 end
